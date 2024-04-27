b5d466cc80c188378f28a155a67aae66accef997
Mohan <kmohan0910@gmail.com>
2024-04-28 00:32:06 +0530

    Removed Comments & Added Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'weather-app';
   constructor(){
-  //   navigator.setAppBadge(42).then(() => {
-  //     console.log("The badge was added");
-  // }).catch((e:any) => {
-  //     console.error("Error displaying the badge", e);
-  // });
   }
 }
